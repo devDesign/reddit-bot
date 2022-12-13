81615a0e4e8f2851be2b8701913a04d0e57f527c
Your Name <you@example.com>
2022-12-12 22:57:10 -0800

    no img gen untill its free

--- api/get-topic/index.ts
+++ api/get-topic/index.ts
@@ -36,15 +36,15 @@ export default async function handler(request, context) {
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `summerize the plot to this story and make up a setting. https://reddit.com${permalink}`,
-    max_tokens: 100
+    prompt: `summerize the plot to this story and make up a setting. dont give away the ending. https://reddit.com${permalink}`,
+    max_tokens: 200
   });
 
   const text = response.data.choices[0].text
 
   const titleResponse = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `come up with the title this book. no quotes. the plot: ${text}`,
+    prompt: `respond with a book title. \n\nthe plot: ${text}.`,
     max_tokens: 100
   });
 
@@ -52,19 +52,22 @@ export default async function handler(request, context) {
 
   const promptResponse = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `summarize this plot into a description of a  photo. explain the subject, how it looks and the lighting and colors. the plot:  ${text}`,
+    prompt: `describe a  photo of  this plot. the lighting and colors, how its arranged, what style to use. add something distantly related to the plot in the photo that doesn't contain words. chose the artistic effect that relates to the plot . add an extra object that is directly related as an analogy. \n\nsummarize the describe in a table \n\nthe plot:  \n\n${text}\n\n|EVENT|SETTING|COLOR|FOREGROUND|BACKGROUND|EXTRA OBJECT|RELATED OBJECT|LENS|RENDER|FRAME|EFFECT|STYLE|\n`,
     max_tokens: 100
   });
 
-  const prompt = promptResponse.data.choices[0].text
+  let prompt = promptResponse.data.choices[0].text.replace(/[^\w|]/g, " ").trim();
+  const elements = prompt.split("|");
+  const array = elements.map(element => element.trim()).filter(element => element !== "");
+  prompt = array.join('|')
 
-  const imageResponse = await openai.createImage({
-        prompt: `${prompt}. Photo, Hyper Detail, 8K, HD, Octane Rendering, Unreal Engine, V-Ray, full hd -- s5000 --uplight --q 3 --stop 80--w 0.5 --ar 1:3 --no-text`,
-        n:1,
-        size:"1024x1024"
-    })
+  // const imageResponse = await openai.createImage({
+  //       prompt: `${prompt}`,
+  //       n:1,
+  //       size:"256x256"
+  //   })
 
-  const imageUrl = imageResponse.data.data[0].url;
+  // const imageUrl = imageResponse.data.data[0].url;
 
-  context.json({'topic': text, 'image': imageUrl, 'title': title })
+  context.json({'topic': text, 'image': prompt, 'title': title })
 }
